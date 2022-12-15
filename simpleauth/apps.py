d82e8d6c068288a17c60063c2ee253ee0a9e1136
Sam Komarov <sam@skomarov.com>
2022-12-15 17:06:43 +0300

    implement login and logout pages

--- simpleauth/apps.py
+++ simpleauth/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AuthapiConfig(AppConfig):
+class SimpleauthConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "authapi"
+    name = "simpleauth"
