fea81f1cf293922c77d5b1377be8f003ef0f2254
Sam Komarov <sam@skomarov.com>
2022-12-15 14:33:26 +0300

    use env variables for secrets and paths

--- ocrvBackendV2/settings/config.py
+++ ocrvBackendV2/settings/config.py
@@ -1,6 +1,17 @@
+import os
 from pathlib import Path
 
-SECRET_KEY = 'idk-my-secret-key'
-TEXTS_PATH = Path("./texts.csv").resolve()
-LOG_PATH = Path("./logs.log").resolve()
-RECORDINGS_DIR = Path("./recordings").resolve()
+from django.core.exceptions import ImproperlyConfigured
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def _get_env(key: str) -> str:
+    val = os.getenv(key)
+    if not val: raise ImproperlyConfigured(f'Please specify {key} as an environment variable.')
+    return val
+
+SECRET_KEY = _get_env('OCRV_RECORDER_SECRET_KEY')
+TEXTS_PATH = Path(_get_env('OCRV_RECORDER_TEXTS_PATH')).resolve()
+LOG_PATH = Path(_get_env('OCRV_RECORDER_LOG_PATH')).resolve()
+RECORDINGS_DIR = Path(_get_env('OCRV_RECORDER_RECORDINGS_DIR')).resolve()
