d4cd8edcfb6979b7d785db5bfacadda43853f44f
Sam Komarov <sam@skomarov.com>
2022-12-16 12:14:06 +0300

    delete the signup form

--- authapi/views.py
+++ authapi/views.py
@@ -7,7 +7,6 @@ from rest_framework.response import Response
 from rest_framework.request import Request
 from rest_framework import status
 
-from authapi import forms
 from authapi.serializers import SignUpSerializer
 
 
