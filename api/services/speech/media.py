806671c99c274ea27463c20496af4674fbc4cea4
Sam Komarov <sam@skomarov.com>
2022-12-19 09:58:18 +0300

    implement removing the speech files in case of an error

--- api/services/speech/media.py
+++ api/services/speech/media.py
@@ -1,16 +1,9 @@
-import os
-import uuid
-
 import ffmpeg
 from django.core.files.uploadedfile import UploadedFile
 
 
-def encode_and_save(media: UploadedFile, tmp_path: str, path: str):
-    with open(tmp_path, 'wb+') as destination:
-        for chunk in media.chunks():
-            destination.write(chunk)
-    ffmpeg.input(tmp_path).output(path).overwrite_output().run()
-    os.remove(str(tmp_path))
+def reencode(input: str, output: str):
+    ffmpeg.input(input).output(output).overwrite_output().run()
 
 def extract_audio(video_path: str, audio_path: str):
     (
@@ -23,7 +16,6 @@ def extract_audio(video_path: str, audio_path: str):
 
 def get_duration(path: str) -> float:
     meta = ffmpeg.probe(path)
-    print(meta)
     stream = meta['streams'][0]
     return float(stream['duration'])
 
--- api/services/speech/service.py
+++ api/services/speech/service.py
@@ -2,8 +2,10 @@ import math
 import os
 import pathlib
 import uuid
+from typing import List
 
 from django.conf import settings
+from django.core.files.uploadedfile import UploadedFile
 
 from . import values, log, media
 from .exceptions import MinDurationException, MaxDurationException
@@ -15,21 +17,36 @@ AUDIO_EXT = 'wav'
 def save_recording(rec: values.Recording) -> None:
     base_filename = f'{rec.user_id}_{rec.text_id}.'
 
-    path = _save_speech_file(rec, base_filename)
-    # there is no need to delete the created files in case of an exception since they will be overridden by a future request
-    _validate_duration_and_log(rec, path)
-    if rec.is_video:
-        _save_audio_from_video(path, base_filename)
+    paths = []
+    try:
+        speech_path = _save_speech_file(rec, base_filename)
+        paths += speech_path
+        _validate_duration_and_log(rec, speech_path)
+        if rec.is_video:
+            paths += _save_audio_from_video(speech_path, base_filename)
+        log.log_success(rec)
+    except Exception as e:
+        map(os.remove, paths)
+        raise e
 
-    log.log_success(rec)
 
 def _save_speech_file(rec: values.Recording, base_filename: str) -> pathlib.Path:
     filename = base_filename + (VIDEO_EXT if rec.is_video else AUDIO_EXT)
     path = settings.RECORDINGS_DIR / filename
     tmp_path = settings.RECORDINGS_DIR / ("tmp_" + filename)
 
-    media.encode_and_save(rec.speech, str(tmp_path), str(path))
+    try:
+        _save_input(rec.speech, tmp_path)
+        media.reencode(str(tmp_path), str(path))
+    finally:
+        os.remove(tmp_path)
     return path
+
+def _save_input(file: UploadedFile, path: str):
+    with open(path, 'wb+') as destination:
+        for chunk in file.chunks():
+            destination.write(chunk)
+
 
 def _save_audio_from_video(video_path: pathlib.Path, base_filename: str) -> None:
     audio_filename = base_filename + AUDIO_EXT
@@ -50,7 +67,6 @@ def validate_duration(text_id: str, media_path: pathlib.Path) -> None:
     text = find_text(text_id)
     if text is None: raise TextNotFound()
     duration = media.get_duration(str(media_path))
-    print(duration)
     if text.min_duration and duration < text.min_duration:
         raise MinDurationException(got=math.floor(duration), want=text.min_duration)
     elif text.max_duration and duration > text.max_duration:
