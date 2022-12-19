c3ff1ec032cbefed1afa48a727a6edbec70bb367
Sam Komarov <sam@skomarov.com>
2022-12-19 20:48:42 +0300

    factor out error handling from the views

--- api/views.py
+++ api/views.py
@@ -3,11 +3,9 @@ from rest_framework.request import Request
 from rest_framework import status
 from rest_framework.decorators import api_view
 
-from common.exceptions import error_response
-from common.time import format_duration
 from . import serializers
 from .di import deps
-from .features import texts, speeches
+from .exceptions import save_recording_with_exceptions
 
 
 @api_view(['GET'])
@@ -30,26 +28,7 @@ def post_speech(request: Request):
     serializer = serializers.RecordingSerializer(data=request.data)
     serializer.is_valid(raise_exception=True)
     rec = serializer.save(by_user_id=str(request.user.id))
-    # TODO: factor out the error handling
-    try:
-        deps.speeches.save_recording(rec)
-    except texts.TextNotFound:
-        return error_response(
-            'Текст с указанным id не найден.',
-            status.HTTP_404_NOT_FOUND
-        )
-    except speeches.MinDurationException as e:
-        return error_response(
-            f'Не соблюдено мин. время начитки.\nТребуется {format_duration(e.want)}, получено {format_duration(e.got)}.',
-            status.HTTP_400_BAD_REQUEST
-        )
-    except speeches.MaxDurationException as e:
-        return error_response(
-            f'Не соблюдено макс. время начитки.\nТребуется {format_duration(e.want)}, получено {format_duration(e.got)}.',
-            status.HTTP_400_BAD_REQUEST
-        )
+    return save_recording_with_exceptions(rec)
 
-
-    return Response(status=status.HTTP_200_OK)
 
 
